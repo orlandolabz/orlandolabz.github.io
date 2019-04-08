8378d71444570ad26e760d45a1dfdedb74f7d819
rionalab <rionalab@gmail.com>
2019-04-08 15:57:13 +0700

    first commit

---
+++ DataProducts.js
@@ -0,0 +1,77 @@
+const DataProducts = {
+    'pertama': [
+        {
+            nama: 'Produk Pertama',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk3.jpg'
+        }, {
+            nama: 'Produk Kedua',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk1.jpg'
+        }, {
+            nama: 'Produk Ketiga',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk2.jpg'
+        }, {
+            nama: 'Produk Keempat',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk4.jpg'
+        }
+    ],
+    'kedua': [
+        {
+            nama: 'Produk Pertama',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk1.jpg'
+        }, {
+            nama: 'Produk Kedua',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk2.jpg'
+        }, {
+            nama: 'Produk Ketiga',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk4.jpg'
+        }, {
+            nama: 'Produk Ketiga',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk3.jpg'
+        },
+    ],
+    'ketiga': [
+        {
+            nama: 'Produk Pertama',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk3.jpg'
+        }, {
+            nama: 'Produk Kedua',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk1.jpg'
+        }, {
+            nama: 'Produk Ketiga',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk2.jpg'
+        }, {
+            nama: 'Produk Ketiga',
+            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+            gambar: 'produk4.jpg'
+        }
+    ],
+    'keempat': [{
+        nama: 'Produk Pertama',
+        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+        gambar: 'produk4.jpg'
+    }, {
+        nama: 'Produk Kedua',
+        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+        gambar: 'produk2.jpg'
+    }, {
+        nama: 'Produk Ketiga',
+        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+        gambar: 'produk3.jpg'
+    }, {
+        nama: 'Produk Ketiga',
+        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
+        gambar: 'produk1.jpg'
+    }],
+}
+
