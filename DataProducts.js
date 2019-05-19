b3348f9cfaa7f6f05046260e493cd972ecc12113
rionalab <rionalab@gmail.com>
2019-05-19 16:32:19 +0700

    last update

--- DataProducts.js
+++ DataProducts.js
@@ -1,77 +1,146 @@
+/*[3:00 PM, 4/24/2019] Mujurmas Owner:  elbow
+                  -Flange
+                  - tee
+                  - Reducer
+                  - Water mur
+                  -Double nepple
+                  -Soket
+[3:07 PM, 4/24/2019] Orlando Ricky: ok pak, itu uda semua ya ?
+[3:36 PM, 4/24/2019] Mujurmas Owner: Ya , sama yg pertama udh kamu buat itu flexible hose stainless steel*/
+
 const DataProducts = {
-    'pertama': [
+    'Flange': [
         {
-            nama: 'Produk Pertama',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk3.jpg'
+            nama: 'Orifice Flange',
+            keterangan: 'generally come with either Raised Faces or RTJ (Ring Type Joint) facings. They are, for all intensive purposes, the same as weld neck and slip-on flanges with extra machining.',
+            gambar: 'orifice-flanges-500x500.jpg'
         }, {
-            nama: 'Produk Kedua',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk1.jpg'
+            nama: 'Long Weld Neck Flange',
+            keterangan: 'Covered in ASME B16.5 – which covers Pipe Flanges and Flanged Fittings for size NPS ½” to 24” for above NPS 26” to 60” it should be as per ASME B16.47.',
+            gambar: 'carbon-steel-long-weld-neck-flange-52-500x500.jpg'
         }, {
-            nama: 'Produk Ketiga',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk2.jpg'
+            nama: 'Blind Flanges',
+            keterangan: 'Used to seal the end of piping systems or pressure vessel openings, and allow easy access to the pipe in case work must be done inside the line. They are often times used for high pressure applications.',
+            gambar: 'stainless-steel-blind-flanges-500x500.jpg'
         }, {
-            nama: 'Produk Keempat',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk4.jpg'
+            nama: 'Weld Neck Pipe Flanges',
+            keterangan: 'Typically provided with a raised face, flat face, or RTJ facing. When a raised face is necessary for weld neck pipe flanges, the standard height is 1/16″ for weld neck pipe flanges under 400#. ',
+            gambar: 'Vale-weld-neck-flange-150-lb.jpg'
         }
     ],
-    'kedua': [
+    'Tee': [
         {
-            nama: 'Produk Pertama',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk1.jpg'
+            nama: 'Stainless  Tee, Structure Pipe And Gas Pipe',
+            keterangan: 'Stainless Steel Reducing Tee, Structure Pipe And Gas Pipe; Application: Structure Pipe, Gas Pipe, Hydraulic Pipe, Chemical Fertilizer Pipe, Pneumatic Connections',
+            gambar: 'stainless-steel-reducing-tee-500x500.jpg'
         }, {
-            nama: 'Produk Kedua',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk2.jpg'
+            nama: '90 Degree Tee Pipe Fitting',
+            keterangan: 'Corrosion Resistance For Boiler Industry',
+            gambar: '90_degree_tee_pipe_fitting_corrosion_resistance_for_boiler_industry.jpg'
         }, {
-            nama: 'Produk Ketiga',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk4.jpg'
+            nama: 'Stainless Steel Socket Weld Tee',
+            keterangan: 'Material Grade 304 304L 316 316L 310 310H; Applications:Oil and Gas, Thermal & Power, Process Instrumentation, Ventilation, etc.',
+            gambar: 'stainless-steel-socket-weld-tee-500x500.jpg'
         }, {
-            nama: 'Produk Ketiga',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk3.jpg'
+            nama: 'Vishal Stainless Steel Forged Threaded Tee',
+            keterangan: 'Thread Size: 3M; Brand: Vishal; Form: S/W And SCRD; Material : Stainless Steel; Grade : 304N',
+            gambar: 'stainless-steel-forged-threaded-tee-500x500.jpg'
         },
     ],
-    'ketiga': [
+    'Reducer': [
         {
-            nama: 'Produk Pertama',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk3.jpg'
+            nama: 'SS Pneumatic Reducer',
+            keterangan: 'We are a unique name in this industry to provide our prestigious clients an exclusive range of SS Pneumatic Reducer.',
+            gambar: 'ss-pneumatic-reducer-500x500.jpg'
         }, {
-            nama: 'Produk Kedua',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk1.jpg'
+            nama: 'Brass Pneumatic Reducer',
+            keterangan: 'Hydraulic Pipe,Gas Pipe,Structure Pipe; Surface Finishing:  Polished; Material: Brass',
+            gambar: 'brass-pneumatic-reducer-500x500.jpg'
         }, {
-            nama: 'Produk Ketiga',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk2.jpg'
+            nama: 'Butt Weld Reducer, Size: 1/8-48 Inch',
+            keterangan: 'Type: Seamless / ERW / Welded, ANSI B 16.9 ,Also Available Dimension Standard B 16.25 , B 16.28,MSS SP-43,SP-48,SP59',
+            gambar: 'butt-weld-reducer-500x500.jpg'
         }, {
-            nama: 'Produk Ketiga',
-            keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-            gambar: 'produk4.jpg'
+            nama: 'Brass Hex Reducer',
+            keterangan: 'Natural, Nickel, Tin, Gold, Silver or any coating as per specification. Any special brass material composition as per customers requirement.',
+            gambar: 'brass-hex-reducer-500x500.jpg'
         }
     ],
-    'keempat': [{
-        nama: 'Produk Pertama',
-        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-        gambar: 'produk4.jpg'
-    }, {
-        nama: 'Produk Kedua',
-        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-        gambar: 'produk2.jpg'
-    }, {
-        nama: 'Produk Ketiga',
-        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-        gambar: 'produk3.jpg'
-    }, {
-        nama: 'Produk Ketiga',
-        keterangan: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio eligendi enim unde debitis eum asperiores amet odit cumque dolor animi.',
-        gambar: 'produk1.jpg'
-    }],
+    'Water Mur': [
+        {
+            nama: 'watermur 3/4" galvanis/ water mur 3/4" besi',
+            keterangan: '3/4"; galvanis; drat keduanya.',
+            gambar: '16403067_6078a37d-5bf2-4c7b-8b8d-365f8c84d1f6_614_460.jpg'
+        }, {
+            nama: 'WATER MUR PVC',
+            keterangan: 'Water mur ini biasa digunakan pada pemasangan pompa sumur dangkal untuk mengecilkan saluran hisap dari mesin pompa 1 inci ke dalam pipa ukuran 1/2 inci',
+            gambar: 'VLOK_WATER_MUR_PVC_DRAT_LUAR_1_X_1_2_WATER_MUR_POMPA_UNION_S.png.webp'
+        }, {
+            nama: 'WATER MUR BESI GALVANIS 6 INCI',
+            keterangan: 'Water mur besi galvanis; Koneksi drat dalam standard pipa pada kedua sisi nya; Material besi dilapis galvanis; Cocok untuk aneka kebutuhan instalasi air, angin, konstruksi, industri hingga rumah tangga',
+            gambar: '2017_12_29T12_04_27_07_00.jpg.webp'
+        }, {
+            nama: 'Union Watermur',
+            keterangan: 'Tersedia berbagai jenis material Union Water Mur seperti Mild Steel dan Stainless Steel serta Connection BSPT Threaded dan Ansi 150#',
+            gambar: 'Jual-Union-Watermur-Karindo-Abadi-Makmur.jpg'
+        }
+    ],
+    'Double Nepple': [
+        {
+            nama: 'Stainless Steel Double Nipple, Size: 3 Inch , For Structure Pipe',
+            keterangan: 'Size : 3 inch; Application : Structure Pipe; Thread Size : 2mm; Head Code :  Round; Length : Sch40.',
+            gambar: 'stainless-steel-double-nipples-500x500.jpg'
+        }, {
+            nama: 'Double Nepple Chrome 3/4" ONDA ',
+            keterangan: 'Double Nepple Double Drat 3/4"; 100% Chrome Plated Brass; 100% Merk Onda ASLI; Kuat & Tahan Lama',
+            gambar: '20363381_f5c3e124-bac6-42cc-8ec8-4d25dcc24d07_800_800.jpg'
+        }, {
+            nama: 'Fitting Double Nepple Elbow Drat Tee Drat',
+            keterangan: 'Tersedia juga tipe : Drat Dalam / Drat Luar / Kombinasi Drat Dalam x Luar; Bahan Besi dan Stainless Steel',
+            gambar: 'b24ef09751511d6fbbedee6416e9dbe6_440x440.jpg'
+        }, {
+            nama: 'Double Nepple 2 Inch 2" Drat Galvanis',
+            keterangan: 'Doubel nipple Galvanis; Tersedia Ukuran: 1/2 inch, 3/4 inch, 1 inch, 1.1/4 inch, 1.1/2 inch, 2 inch',
+            gambar: '0_8729c9d5-79f9-4905-adda-ff83b06eb667_720_698.jpg'
+        }
+    ],
+    'Socket': [
+        {
+            nama: 'BLUE 240V 32 AMP 3 PIN INDUSTRIAL SITE PLUG & SOCKETS IP44 2P+E MALE/FEMALE',
+            keterangan: 'NDUSTRIAL SOCKET RANGE BLUE 240V 32A HEAVY DUTY IMPACT RESISTANT WITH A SPLASH PROOF RATING OF IP44 MAINLY USED FOR SITE WORK, WORKSHOPS AND WHAREHOUSE UNITS',
+            gambar: 'Custom800.jpg'
+        }, {
+            nama: '125A Industrial Socket Outlet , Waterproof Plug And Socket With Cover Protection',
+            keterangan: 'Our valued customers can purchase from us an extensive assortment of Industrial Connector. These products are manufactured by taking optimum quality materials, according to the industry accepted models.',
+            gambar: 'pl16558946-125a_industrial_socket_outlet_waterproof_plug_and_socket_with_cover_protection.jpg'
+        }, {
+            nama: 'Gewiss Multi Socket Shore Power Adaptor GW64010',
+            keterangan: 'The Gewiss multi socket shore power adaptor GW64010 is ideal for busy marina power points. Double outlet socket with plug. 16amp 230v',
+            gambar: '0000023826.jpg'
+        }, {
+            nama: 'IP44 2p+E 16A 32A 220V Multi-Function Industrial Plug and Socket 1013',
+            keterangan: 'Grounding: Standard Grounding; Type:   Without Switch;Rated Current:  32A; Shell Material: ABS PA66; Application:    Industrial; Function:   Ordinary Socket',
+            gambar: 'IP44-2p-E-16A-32A-220V-Multi-Function-Industrial-Plug-and-Socket-1013.jpg'
+        }
+    ],
+    'Hose': [
+        {
+            nama: 'Flex-Flyte® L-9 Hose',
+            keterangan: 'This is a two-ply silicone coated fiberglass hose with a helically wound spring steel wire imbedded between the two-plies.',
+            gambar: '51507p.jpg'
+        }, {
+            nama: 'UFD Clear Polyurethane Hose',
+            keterangan: 'This thermoplastic polyurethane hose reinforced with a wire helix is commonly used in chemical plants, paper mills, saw mills, sheet metal houses, flour and grain mills, and manufacturing facilities',
+            gambar: '51523p.jpg'
+        }, {
+            nama: 'RFH-W Rubber Hose',
+            keterangan: 'This hose is a thermoplastic rubber reinforced with a wire helix and an external polypropylene wearstrip',
+            gambar: '51533p.jpg'
+        }, {
+            nama: 'Vac-U-Flex® TPU Hose',
+            keterangan: 'This hose is made of 100% clear two-ply thermoplastic polyurethane material with a coated spring steel helix encapsulated and bonded to the hose jacket',
+            gambar: '51551p.jpg'
+        }
+    ],
 }
 
